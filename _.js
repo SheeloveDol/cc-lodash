613ab9fedd61b45f5ec2ad5978759ba2ee2b2d78
Sheelove Dol <siesel26@gmail.com>
2021-10-13 16:33:46 -0700

    Implement clamp(), inRange(), words() and pad()

--- _.js
+++ _.js
@@ -1,4 +1,42 @@
+const _ = {
 
+  clamp(number, lower, upper) {
+    var lowerClampedValue = Math.max(number, lower);
+    var clampedValue = Math.min(lowerClampedValue, upper);
+    return clampedValue;
+  },
+
+  inRange(number, start, end) {
+    if (end === undefined) {
+      end = start;
+      start = 0;
+    }
+    if (start > end) {
+      let temp = end;
+      end = start;
+      start = temp;
+    } 
+    let isInRange = start <= number && number < end 
+    return isInRange;
+  },
+
+  words(string) {
+    let words = string.split(' ');
+    return words;
+  },
+
+  pad(string, length) {
+    if (string.length >= length) {
+      return string;
+    }
+    let padStart = Math.floor((length - string.length) / 2);
+    let padEnd = length - (string.length + padStart);
+    let paddedString = ' '.repeat(padStart) + string + ' '.repeat(padEnd);
+    return paddedString;
+  },
+
+
+}
 
 
 
